82d836c38f618867b13707ca4d294f6c23a59c09
Alex Baizeau <abaizeau@freshbooks.com>
2015-12-13 09:47:14 -0500

    2nd challenge

--- day2.js
+++ day2.js
@@ -1,20 +1,42 @@
+"use strict";
 const chalk = require('chalk');
 
 const fs = require('fs');
-var boxDimensions = [];
+let boxDimensions = [];
+
+/**
+ * First challenge
+ */
 fs.readFile('day2/input.txt', 'utf8', (error, data) => {
   boxDimensions = data.split('\n');
-  var  total = boxDimensions.reduce((previousValue, currentValue) => {
+  let  total = boxDimensions.reduce((previousValue, currentValue) => {
     if(!currentValue) {
       return previousValue;
     }
-    const l = currentValue.split('x')[0];
-    const w = currentValue.split('x')[1];
-    const h = currentValue.split('x')[2];
-    result = previousValue + 2*(l*w + l*h + h*w) + Math.min(l*w, l*h, w*h);
-    return result;
+    const l = Number(currentValue.split('x')[0]);
+    const w = Number(currentValue.split('x')[1]);
+    const h = Number(currentValue.split('x')[2]);
+    return previousValue + 2*(l*w + l*h + h*w) + Math.min(l*w, l*h, w*h);
   }, 0);
 
   console.log(chalk.green(`challenge 1: ${total}`));
 });
 
+/**
+ * 2nd challenge
+ */
+fs.readFile('day2/input.txt', 'utf8', (error, data) => {
+  boxDimensions = data.split('\n');
+  let  total = boxDimensions.reduce((previousValue, currentValue) => {
+    if(!currentValue) {
+      return previousValue;
+    }
+    const l = Number(currentValue.split('x')[0]),
+      w = Number(currentValue.split('x')[1]),
+      h = Number(currentValue.split('x')[2]),
+      volume = l*h*w,
+      smallestPerimeter = Math.min(2*(l+w), 2*(l+h), 2*(w+h));
+    return previousValue + volume + smallestPerimeter;
+  }, 0);
+  console.log(chalk.green(`challenge 2: ${total}`));
+});
